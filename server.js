86a002f2552f1399e2756c4d137892473aaf430b
anujpatel5123 <anujpat3l@gmail.com>
2023-02-05 20:21:26 -0500

    updated server.js

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const {initialize, getAllPosts, getPublishedPosts, getCategories} = require("./blog-service")
+
+app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'views')));
+var HTTP_PORT = process.env.PORT || 8080;
+
+
+app.get("/", (req,res)=>{
+    res.redirect("/about");
+})
+
+
+app.get("/about", (req,res)=>{
+    res.sendFile(__dirname + '/views/about.html');
+})
+
+
+//blog route
+app.get("/blog", (req, res) => {
+    getPublishedPosts()
+    .then((data) => {
+      res.send(data)
+    })
+    // Error Handling
+    .catch((err) => {
+      res.send(err);
+    })
+  });
+
+
+
+//posts route
+app.get("/posts", (req, res) => {
+    getAllPosts()
+      .then((data) => {
+        res.send(data)
+      })
+      // Error Handling
+      .catch((err) => {
+        res.send(err);
+      })
+  });
+
+
+//Categories Route
+app.get("/categories", (req, res) => {
+    getCategories()
+    .then((data) => {
+      res.send(data)
+    })
+    // Error Handling
+    .catch((err) => {
+      res.send(err);
+    })
+  })
+
+
+
+// ERROR 404
+
+
+initialize().then(() => {
+    // Start the server after the files are read and the initialization is done
+    app.listen(HTTP_PORT, () => {
+      console.log("Express http server listening on: " + HTTP_PORT);
+    });
+})
